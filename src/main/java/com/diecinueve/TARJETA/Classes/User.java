3a35aaad4dabfad6fa02cfca9e30827f802667c8
albertomendi <alberto.mendi@hotmail.com>
2016-11-20 22:33:16 +0100

    Database operations: prepared private methods for Shop. Added a user method, added hash.

--- src/main/java/com/diecinueve/TARJETA/Classes/User.java
+++ src/main/java/com/diecinueve/TARJETA/Classes/User.java
@@ -8,17 +8,33 @@ public class User {
 	String nick;
 	String password;
 	
+	public boolean login(String nick, String password) throws Exception{
+		checkStringNotNull(nick, password);
+		int passwordHash = password.hashCode();
+		return DataBaseOperations.login(nick, passwordHash);
+	}
+	
 	public void altaUsuario(String nick, String password) throws Exception{
-		if(nick.isEmpty() || password.isEmpty())
-			throw new Exception("Nick or pwd empty");
-		else if(password.length() < 8 || !password.matches(".*\\d.*"))
+		checkStringNotNull(nick, password);
+		if(password.length() < 8 || !password.matches(".*\\d.*"))
 			throw new Exception("pwd needs 8 caracters and a number");
-		DataBaseOperations.createUser(nick, password);
+		int passwordHash = password.hashCode();
+		DataBaseOperations.createUser(nick, passwordHash);
 	}
 	
 	public void bajaUsuario(String userNick) throws Exception{
-		if(nick.isEmpty() || password.isEmpty())
-			throw new Exception("Nick or pwd empty");
+		checkStringNotNull(userNick);
 		DataBaseOperations.deleteUser(userNick);
+	}
+	
+	
+	private void checkStringNotNull(String str1) throws Exception{
+		if(str1.isEmpty())
+			throw new Exception("Nick or pwd empty");
+	}
+	
+	private void checkStringNotNull(String str1, String str2) throws Exception{
+		if(str1.isEmpty() || str2.isEmpty())
+			throw new Exception("Nick or pwd empty");
 	}
 }
--- src/main/java/com/diecinueve/TARJETA/DatabaseOp/DataBaseOperations.java
+++ src/main/java/com/diecinueve/TARJETA/DatabaseOp/DataBaseOperations.java
@@ -1,22 +1,135 @@
 package com.diecinueve.TARJETA.DatabaseOp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class DataBaseOperations {
 
-	public static void createUser(String user, String password) throws Exception{
-		if (checkUserExists(user))
+	//*****************************INITIALIZE VARIABLES******************************
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost/EMP";
+
+	//Credenciales Base de Datos
+	static final String USER = "root";
+	static final String PASS = "root";
+	private Connection conn = null;
+	private PreparedStatement stmt = null;
+
+
+	//*****************************LOGIN METHODS************************************
+	public static boolean login(String userNick, int password) throws Exception{
+		if (checkUserExists(userNick))
 			throw new Exception("Usuario ya existe");
-		//INSERT INTO Database User user, pwd)
+		//ver si coincide userNick con password, recordar password.toString();
+
+		return true;
 	}
-	
+
+
+	//*****************************USER METHODS************************************
+	public static void createUser(String userNick, int password) throws Exception{
+		if (checkUserExists(userNick))
+			throw new Exception("Usuario ya existe");
+		//INSERT INTO Database User user, pwd), redordar password.toString()
+	}
+
 	public static void deleteUser(String userNick) throws Exception{
 		if(!checkUserExists(userNick))
 			throw new Exception("Usuario doesn't exist");
 		//remove user where name = userNick
 	}
 
+	
+	//*****************************SHOP METHODS************************************
+	public void altaTienda(String shopName) throws Exception{
+		if(!checkShopExists(shopName))
+			throw new Exception("Tienda ya existe");
+		InsertMethod("Tiendas", shopName);
+	}
 
+
+	//*****************************PRIVATE METHODS************************************
+
+	private void connect(){//Metodo para iniciar una conexion a la Base de Datos
+		try{
+
+			//Conexion con Base de Datos
+			Class.forName(JDBC_DRIVER);
+			this.conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+		}
+		catch(Exception e){
+			//Hacer nada o sacar algun tipo de mensaje de error
+		}
+	}
+
+	private void InsertMethod(String WHERE, String VALUES){
+		this.connect();
+		//Preparo la query
+		String query = "INSERT INTO " +WHERE+
+				" VALUES ("+VALUES+")";
+		try {
+			stmt = conn.prepareStatement(query);
+
+			//Ejecuto la query
+			stmt.executeUpdate();
+		}
+		catch (Exception e) {
+			//Indicar error
+		}
+		finally{
+			//Cierro la conexion con la Base de Datos en cualquier caso
+			try{
+				if(stmt != null)
+					stmt.close();
+
+				if(conn!=null)
+					conn.close();
+			}
+			catch(Exception e){
+				//Hacer nada
+			}
+		}
+	}
+	
+	private void DeleteMethod(String FROM, String WHERE, String IS){
+		this.connect();
+		//Preparo la query
+		String query = "DELETE * FROM " +FROM+
+				" WHERE "+ WHERE +"='"+IS+"'";
+		try {
+			stmt = conn.prepareStatement(query);
+
+			//Ejecuto la query
+			stmt.executeUpdate();
+		}
+		catch (Exception e) {
+			//Indicar error
+		}
+		finally{
+			//Cierro la conexion con la Base de Datos en cualquier caso
+			try{
+				if(stmt != null)
+					stmt.close();
+
+				if(conn!=null)
+					conn.close();
+			}
+			catch(Exception e){
+				//Hacer nada
+			}
+		}
+	}
+	
+	
 	private static boolean checkUserExists(String user) {
 		//return ( SELECT * FROM database WHERE nick).equals(user))
+		return true;
+	}
+	
+	private static boolean checkShopExists(String shop) {
+		//return ( SELECT * FROM database WHERE shop).equals(shop))
 		return true;
 	}
 }
