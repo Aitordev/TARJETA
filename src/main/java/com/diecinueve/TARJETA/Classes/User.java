1ba5aad959bca4289817bfc8beae6d5b9336573c
albertomendi <alberto.mendi@hotmail.com>
2016-11-20 20:56:11 +0100

    Add user prototype for the class

---
+++ src/main/java/com/diecinueve/TARJETA/Classes/User.java
@@ -0,0 +1,13 @@
+package com.diecinueve.TARJETA.Classes;
+
+public class User {
+	
+	int idUser;
+	String nick;
+	String password;
+	
+	public void altaUsuario(String nick, String password){
+		
+	}
+
+}
